dd77a76d06018c105e68d2789d4a043ab7017086
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-09-17 22:17:46 +0600

    fake data created using chatGPT

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import NavBar from './components/NavBar/NavBar'
 
 function App() {
 
   return (
     <>
-
+      <NavBar></NavBar>
       <h1 className='text-7xl bg-rose-500'>Vite + React</h1>
 
 
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const NavBar = (props) => {
+
+    const routes = [
+        { "id": 1, "title": "Home", "content": "Welcome to our homepage.", "path": "/" },
+        { "id": 2, "title": "AboutUs", "content": "Learn more about our company.", "path": "/about" },
+        { "id": 3, "title": "Services", "content": "Explore the services we offer.", "path": "/services" },
+        { "id": 4, "title": "Contact", "content": "Get in touch with us.", "path": "/contact" },
+        { "id": 5, "title": "Blog", "content": "Read our latest blog posts.", "path": "/blog" },
+        { "id": 6, "title": "NotFound", "content": "404 Not Found.", "path": "*" }
+    ];
+
+
+    const { } = props
+
+    return (
+        <>
+            <h1>NavBar</h1>
+        </>
+    )
+}
+
+NavBar.propTypes = {
+
+}
+
+export default NavBar
