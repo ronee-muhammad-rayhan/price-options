c9a390afbce9beb1dae31614435adb73258589eb
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-09-17 23:08:12 +0600

    proptypes added

--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 function Link({ route }) {
     return (
@@ -8,6 +9,10 @@ function Link({ route }) {
             </li>
         </>
     )
+}
+
+Link.propTypes = {
+    route: PropTypes.object.isRequired
 }
 
 export default Link
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -25,7 +25,7 @@ const NavBar = (props) => {
 }
 
 NavBar.propTypes = {
-
+    props: PropTypes.array
 }
 
 export default NavBar
